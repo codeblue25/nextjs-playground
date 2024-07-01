2c93e5affea00561e04f7bde0edf20e72871de13
codeblue25 <chois1031@gmail.com>
2024-07-01 23:13:54 +0900

    feat: Server-side data fetching

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,25 @@
-"use client";
+import { Metadata } from "next";
 
-import { useEffect, useState } from "react";
+export const metadata: Metadata = {
+  title: "Home",
+};
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState();
-  const getMovies = async () => {
-    const response = await fetch(
-      "https://nomad-movies.nomadcoders.workers.dev/movies"
-    );
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  // console.log("hi i'm here !");
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function Page() {
+  const movies = await getMovies();
   return (
     <div>
       <h1>Hello NextJS !</h1>
-      {isLoading ? "Loading..." : JSON.stringify(movies)}
+      {JSON.stringify(movies)}
     </div>
   );
 }
